627b7d0b87669ab818b167f96f628b324f5d055f
Colton B <coltonbaldwin101@gmail.com>
2026-05-05 12:39:30 -0400

    Setting up the project and connecting it to the github repository.

---
+++ Reddit_Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className="mainBody">
+          <h1>Yo</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
