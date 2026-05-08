f708686a163cca0879e3964547d46ef38d4d82fe
Colton B <coltonbaldwin101@gmail.com>
2026-05-08 12:29:27 -0400

    fixing the styling in SearchBar.css to be better organized. Included a search bar button and changed the layout of the search bar. Included a state for the value of the search bar that updates whenver user types in it.

--- Reddit_Project/src/components/NavBar/SearchBar/SearchBar.jsx
+++ Reddit_Project/src/components/NavBar/SearchBar/SearchBar.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./SearchBar.css"
+import { useState, useEffect } from "react";
 
 
 function SearchBar() {
+    const [searchBarInput, setSearchBarInput] = useState("");
+
+    function handleChange(event) {
+        setSearchBarInput(event.target.value)
+        console.log(searchBarInput);
+    }
+
     return (
-        <div className="searchBar_holder">
-            <div className="inner">
-                <input className="searchBar" type="text" placeholder="Search for Cool Stuff Here"/>
+        <div className="searchBarHolder">
+            <div className="searchBar">
+            <input  
+                type="text" 
+                placeholder="Search for Cool Stuff Here" 
+                value={searchBarInput} 
+                onChange={handleChange}
+                sub
+            />
+            <button className="button">Search</button>
             </div>
         </div>
     )
